8fd60573f7514c607caa2635172364b9ed0441a3
Adam <adam@adamjwolf.com>
2013-11-15 16:56:24 -0700

    Starting Pixel page Name Script

---
+++ PixelTitles.ps1
@@ -0,0 +1,25 @@
+﻿
+
+Write-Host "  _____ _          _   _______ _ _   _           "
+Write-Host " |  __ (_)        | | |__   __(_) | | |          "
+Write-Host " | |__) |__  _____| |    | |   _| |_| | ___  ___ "
+Write-Host " |  ___/ \ \/ / _ \ |    | |  | | __| |/ _ \/ __|"
+Write-Host " | |   | |>  <  __/ |    | |  | | |_| |  __/\__ \"
+Write-Host " |_|   |_/_/\_\___|_|    |_|  |_|\__|_|\___||___/"                                               
+
+#Add-PSSnapin SqlServerCmdletSnapin100
+#Add-PSSnapin SqlServerProviderSnapin100
+
+$server = "server"
+$database = "database"
+
+
+Function main
+{
+   
+}
+
+
+clear 
+
+main
